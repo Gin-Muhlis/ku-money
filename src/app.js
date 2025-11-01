ae021b97b129c60d5bd1427f0699119d49cc4ea9
Gin Muhlis <ginxrpl07@gmail.com>
2025-11-01 19:14:22 +0700

    fix: cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Apply general rate limiting to all API routes
